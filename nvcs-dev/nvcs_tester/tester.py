008615242c0e84a3f12b945cdb4b8c3a2a6c5d11
Kelvyn Meyers <Kelvyn.Meyers@usda.gov>
2024-03-08 14:51:14 -0800

    Allowed tester.py to work with text and SQLite inputs
    Since the eastern code hasn't yet made it to a SQLite input system yet, I made a few minor changes to allow for reading via read_sqlite() and read_file() depending on the parameters passed.

--- nvcs-dev/nvcs_tester/tester.py
+++ nvcs-dev/nvcs_tester/tester.py
@@ -14,23 +14,24 @@ import key_western_us as classification_key
 #import key_eastern_us as classification_key
 #import key_alaska_us as classification_key
 from classifier import Classifier
-from plot_reader import read_sqlite
+from plot_reader import read_sqlite, read_file
 import logging
 from time import process_time
 
-def run(dbfile, plottbl, outfile):
+def run(outfile, dbfile = None, plottbl = None, txtfile = None):
 
-    print('Input file = %s' % dbfile)
+    print('Input file = %s' % (dbfile if dbfile is not None else txtfile))
     print('Output file = %s' % outfile)
 
     #Create NVC plot classification key
     classifier = Classifier(classification_key.ClassificationKey())
     
-    #Classify plots read from a text file
     logging.basicConfig(filename='C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_output/debug.log', filemode='w', level=logging.DEBUG) # or ERROR
+    #Classify plots read from a sqlite db or text file
     with Timer() as t:
         with open(outfile, 'w') as f:
-            for plot in read_sqlite(dbfile, plottbl):
+            plots = read_sqlite(dbfile, plottbl) if txtfile is None else read_file(txtfile)
+            for plot in plots:
                 solution = classifier.classify(plot)
                 if solution.path[-1]:
                     f.write('{0}\t{1}\t{2}\n'.format(solution.obj.ident, solution.path[-1].ident, str([node.ident for node in solution.path])))
@@ -50,11 +51,13 @@ class Timer:
 
 if __name__ == '__main__':
     import tester
-    #tester.run('C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_input/western-us-test-dataset-1.txt', 'C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_output/western-us-test-dataset-1.out')
-    #tester.run('C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_input/nvcs_python_input_pnwrs_rmrs_2017_utf-8.txt', 'C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_output/nvcs_pnwrs_rmrs_2017.out')
-    # new test dataset:
-    #tester.run('C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_input/nvcs_key_test_data_2017_20240129.txt', 'C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_output/nvcs_pnwrs_rmrs_2017.out')
-    # sqlite db
-    tester.run('C:/ctoney/FIA/nvcs/rmrs-data/nvcs_west_test_data_2017.db', 'NVCS_KEY_TEST_DATA', 'C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_output/nvcs_pnwrs_rmrs_2017.out')
-    #tester.run('C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_input/nvcs_key_test_data_2017_20230404.tsv', 'C:/ctoney/FIA/nvcs/nvcs-workspace/nvcs_west/nvcs_tester/run_output/nvcs_pnwrs_rmrs_2017_newdata.out')
+
+    # sqlite-db input
+    tester.run(outfile=r'C:\Users\kelvynmeyers\Documents\GitHub\FS-Enterprise\NVCS\nvcs-dev\nvcs_tester\run_output\west\nvcs_pnwrs_rmrs_2017.out',
+               dbfile=r'C:\Users\kelvynmeyers\Documents\GitHub\FS-Enterprise\NVCS\nvcs-dev\nvcs_tester\run_input\west\sqllite_db\nvcs_west_test_data_2017.db',
+               plottbl='NVCS_KEY_TEST_DATA')
+
+    # # txt input
+    # tester.run(outfile=r'C:\Users\kelvynmeyers\Documents\GitHub\FS-Enterprise\NVCS\nvcs-dev\nvcs_tester\run_output\east\nvcs_dataset_2009.out',
+    #            txtfile=r'C:\Users\kelvynmeyers\Documents\GitHub\FS-Enterprise\NVCS\nvcs-dev\nvcs_tester\run_input\east\text_files\nvc-dataset-2009-MODIFIED.txt')
     pass
