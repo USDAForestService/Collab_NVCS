cb057863360c841ba498203db71796de03ef76ea
Kelvyn Meyers <72825162+Kelvyn-Meyers@users.noreply.github.com>
2025-01-10 12:17:07 -0800

    Began development of the Document Editor dialog
    To avoid hard-coded text situations, we'll need a dialog that can build document sections with headers, text, and element descriptions dynamically.

--- nvcs-client/src/document.js
+++ nvcs-client/src/document.js
@@ -1,3 +1,46 @@
+let documentStructure = {
+    sections: [
+        {
+            name: "sectionOne",
+            content: [
+                {
+                    type: "header",
+                    level: "1",
+                    content: "SECTION I. KEY TO PLANTATION VS. NATURAL [WESTERN SECTION]"
+                },
+                {
+                    type: "text",
+                    content: "A summary of the forest plantation types is provided in Table xx."
+                },
+                {
+                    type: "text",
+                    content: "[work with FIA staff to determine what is needed for plantation types in west.]"
+                },
+                {
+                    type: "text",
+                    content: "Forest types are separated first into plantation versus natural types. FIA field crews " +
+                    "make that call in the field, and we rely entirely on that call to make the distinction. The " +
+                    "plantation label is sometimes applied to failed plantations that may now be dominated by " +
+                    "non-plantation species (typically hardwoods), to stands in very early stages of planting (and thus " +
+                    "dominated by native conifers or hardwoods) or to transitional areas that are on the edge of the " +
+                    "plantation. Likewise, failed attempts to establish plantations may become obscured following " +
+                    "subsequent natural regeneration, in such cases, FIA field crews observe no plantation evidence and " +
+                    "label stand origin as natural regeneration, possibly conflicting with, or updating, historical stand " +
+                    "records (which are not utilized in this classification)."
+                },
+                {
+                    type: "element",
+                    hierarchyName: "Forest Plantations"
+                },
+                {
+                    type: "element",
+                    hierarchyName: "Natural (including ruderal) Forests"
+                }
+            ]
+        }
+    ]
+};
+
 function toggleDocumentForm() {
     const documentForm = document.getElementById("document-form");
     if (documentForm.hidden)
@@ -23,6 +66,133 @@ function hideDocumentForm() {
 
     // Show JSON tree
     showContentById("detected-json-container");
+}
+
+function openDocumentDialog() {
+    const dialog = document.getElementById("document-dialog");
+    populateDocumentDialog(dialog);
+    showDialog(dialog);
+}
+
+function populateDocumentDialog(dialog) {
+    let html = "";
+
+    for (const section of documentStructure.sections) {
+        // New section prep
+        const identifier = newGuid();
+        html += "<div class='document-section'>";
+
+        // Section Name
+        html += `
+            <div class='input-container'>
+                <label for='section-name'>Section Name:</label>
+                <input id='section-name' type='text' value='${section.name}' />
+                <button id='btn-delete-seciton'>Delete</button>
+            </div>
+        `;
+
+        // Existing Content
+        html += "<div class='document-section-content'>";
+        for (const content of section.content)
+            html += generateDocumentEditorContent(content)
+        html += "</div>";
+
+        // Add Content Settings
+        html += `
+            <div>
+                <button id='btn-add-document-header-${identifier}'>Add Header</button>
+                <button id='btn-add-document-text-${identifier}'>Add Text</button>
+                <button id='btn-add-document-element-${identifier}'>Add Element Description</button>
+            </div>
+        `;
+
+        html += "</div>";
+    }
+
+    // Add Section Settings
+    html += `
+        <div class='input-container'>
+            <label for='add-section-name'>New Section Name:</label>
+            <input id='add-section-name' type='text' />
+            <button id='add-section'>Add Section</button>
+        </div>
+    `;
+
+    // Apply HTML
+    const body = dialog.querySelector(".body-container");
+    body.innerHTML = html;
+}
+
+function generateDocumentEditorContent(item) {
+    let html = "";
+
+    switch (item.type) {
+        case "element":
+            html += generateDocumentEditorElementContent(item);
+            break;
+        case "header":
+            html += generateDocumenEditortHeaderContent(item);
+            break;
+        case "text":
+            html += generateDocumentEditorTextContent(item);
+            break;
+        default:
+            throw new Error("Invalid type provided", item.type);
+    }
+
+    return html;
+}
+
+function generateDocumentEditorElementContent(item) {
+    const element = hierarchy.filter(i => i.hierarchyName = item.hierarchyName)[0];
+    const hierarchyOptions = generateElementOptions(hierarchy, element);
+    const identifier = newGuid();
+
+    let html = `
+        <div class='input-container'>
+            <label for='element-source-${identifier}'>Element Description Source:</label>
+            <select id='element-source-${identifier}'>
+                ${hierarchyOptions}
+            </select>
+        </div>
+    `;
+
+    return html;
+}
+
+function generateDocumenEditortHeaderContent(item) {
+    const identifier = newGuid();
+    let levelOptions = "";
+    for (let i = 1; i <= 6; i++) {
+        const selected = item.level == i ? "selected" : "";
+        levelOptions += `<option value="${i}" ${selected}>${i}</option>`;
+    }
+
+    let html = `
+        <div class='input-container'>
+            <label for='header-content-${identifier}'>Header Content:</label>
+            <input id='header-content-${identifier}' type="text" value='${item.content}'/>
+            <label for='header-level-${identifier}'>Header Level:</label>
+            <select id='header-level-${identifier}'>
+                ${levelOptions}
+            </select>
+        </div>
+    `;
+    
+    return html;
+}
+
+function generateDocumentEditorTextContent(item) {
+    const identifier = newGuid();
+
+    let html = `
+        <div class='input-container'>
+            <label for='text-content-${identifier}'>Text Content:</label>
+            <textarea id='text-content-${identifier}'>${item.content}</textarea>
+        </div>
+    `;
+    
+    return html;
 }
 
 function generateDocument() {
