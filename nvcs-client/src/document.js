94f28e9f5dd9189792c18cf0bb24de4429de3597
Kelvyn Meyers <72825162+Kelvyn-Meyers@users.noreply.github.com>
2025-02-03 12:34:00 -0800

    Began implementing document formatting from JSON settings
    Now we'll read the document structure JSON object and use it to build the visual document on the page

--- nvcs-client/src/document.js
+++ nvcs-client/src/document.js
@@ -218,101 +218,158 @@ function generateDocumentEditorTextContent(item) {
 function generateDocument() {
     let html = "";
     
-    html += generateDocumentSectionOne();
-    html += generateDocumentSectionTwo();
-    html += generateDocumentSectionThree();
-    html += generateDocumentSectionFour();
+    for (const section of documentStructure.sections)
+        html += generateDocumentSection(section);
 
     const documentContainer = document.getElementById("document-container");
     documentContainer.innerHTML = html;
 }
 
-function generateDocumentSectionOne() {
-    let html = "<h1>SECTION I. KEY TO PLANTATION VS. NATURAL [WESTERN SECTION]</h1>";
-    html += "<p><b>[HARDCODED]</b> A summary of the forest plantation types is provided in Table xx.</p>" +
-        "<p>[work with FIA staff to determine what is needed for plantation types in west.]</p>" +
-        "<p>Forest types are separated first into plantation versus natural types. FIA field crews make that call in the " +
-        "field, and we rely entirely on that call to make the distinction. The plantation label is sometimes applied to " +
-        "failed plantations that may now be dominated by non-plantation species (typically hardwoods), to stands in very " +
-        "early stages of planting (and thus dominated by native conifers or hardwoods) or to transitional areas that are " +
-        "on the edge of the plantation. Likewise, failed attempts to establish plantations may become obscured following " +
-        "subsequent natural regeneration, in such cases, FIA field crews observe no plantation evidence and label stand " +
-        "origin as natural regeneration, possibly conflicting with, or updating, historical stand records (which are not " +
-        "utilized in this classification).</p>";
-    const designatedElements = hierarchy.filter(i => i.hierarchyLevel == 0);
-    html += "<ul>";
-    for (const element of designatedElements) {
-        html += "<li>";
-        html += generateDocumentDescription(element);
-        html += "</li>";
+function generateDocumentSection(section) {
+    let html = "";
+
+    for (const content of section.content) {
+        switch (content.type) {
+            case "element":
+                html += generateDocumentElement(content);
+                break;
+            case "header":
+                html += generateDocumentHeader(content);
+                break;
+            case "skeletal":
+                html += generateDocumentSkeletal(content, section.content);
+                break;
+            case "text":
+                html += generateDocumentText(content);
+                break;
+            default:
+                throw new Error("Invalid type provided", content.type);
+        }
     }
-    html += "</ul>";
+
     return html;
 }
 
-function generateDocumentSectionTwo() {
-    let html = "<h1>SECTION II. KEY TO FORESTED VS. NON-FORESTED [WESTERN SECTION]</h1>";
-    html += "<p><b>[HARDCODED]</b> IF RIV = 0 AND TTCOV <= 20%, then \"Possibly Regenerating\"</p>";
+function generateDocumentElement(content) {
+    const element = hierarchy.filter(i => i.hierarchyName == content.content)[0];
+    let html = generateDocumentDescriptionByElement(element, content.descendantLimitType);
     return html;
 }
 
-function generateDocumentSectionThree() {
-    let html = "<h1>SECTION III. KEY TO UPLAND VS. WETLAND [WESTERN SECTION]</h1>";
-    const designatedElements = hierarchy.filter(i => i.hierarchyName == "Natural (including ruderal) Forests")[0].children;
-    html += "<ul>";
-    for (const element of designatedElements) {
-        html += "<li>";
-        html += generateDocumentDescription(element);
-        html += "</li>";
-    }
-    html += "</ul>";
-    return html;
-}
-
-function generateDocumentSectionFour() {
-    let html = "<h1>SECTION IV. KEY TO NATURAL UPLAND FOREST & WOODLAND DIVISIONS</h1>";
-    html += "<h2>IV.1. Skeletal Version (upland)</h2>";
-    html += "<p>TODO</p>";
-    html += "<h2>IV.2. Detailed Version (upland)</h2>";
-    html += "<p><b>[HARDCODED]</b>Please read the introduction to understand the approach used in the key.</p>";
-    const designatedRoot = hierarchy.filter(i => i.hierarchyName == "Upland Forest & Woodlands")[0];
-    const designatedChildTypes = ["subclass", "formation", "division"];
-    let designatedChildren = [];
-    for (const child of designatedRoot.children)
-        designatedChildren = getDescendantElementsByType(designatedChildren, child, designatedChildTypes);
-    html += "<ul>";
-    for (const element of designatedChildren) {
-        html += "<li>";
-        html += generateDocumentDescription(element);
-        html += "</li>";
-    }
-    html += "</ul>";
-    return html;
-}
-
-function generateDocumentDescription(element) {
-    let returnString = "<p style='white-space: pre-wrap;'>";
-    let descriptionList = [...element.node.description];
-    const leadingSpaceReplace = "    ";
+function generateDocumentDescriptionByElement(element, descendantLimitType) {
+    const descriptionList = [...element.node.description];
     for (let i = 0; i < descriptionList.length; i++) {
-        descriptionList[i] = descriptionList[i].replace(/^(\s|\\t)+/, function(i) { return i.replace(/\s/g, leadingSpaceReplace); });
+        descriptionList[i] = descriptionList[i].trim();
     }
-    let fullDescription = descriptionList.join("</br>");
-    returnString += fullDescription;
-    returnString += ` ... <button data-hierarchy-name='${element.hierarchyName}' class='hierarchyNodeButton' onclick='openJsonDialog("${element.hierarchyName}")'><b>${element.hierarchyName}</b></button>`;
-    returnString += "</p>";
-    return returnString;
+    const fullDescription = descriptionList.join("</br>");
+    const elementButton = `<button data-hierarchy-name='${element.hierarchyName}' class='hierarchyNodeButton' onclick='openJsonDialog("${element.hierarchyName}")'><b>${element.hierarchyName}</b></button>`;
+
+    let html = `
+        <p style='white-space: pre-wrap;'>${fullDescription} ... ${elementButton}</p>
+    `;
+
+    const designatedTypes = getEligibleTypesByLimit(descendantLimitType);
+    for (const child of element.children) {
+        if (!designatedTypes.includes(child.node.level))
+            continue;
+
+        html += generateDocumentDescriptionByElement(child, descendantLimitType);
+    }
+
+    return html;
 }
 
-function generateDocumentListEntry(element) {
-    let returnString = "<li>";
-    returnString += generateDocumentDescription(element);
-    returnString += "<ul>";
-    for (let child of element.children)
-        returnString += generateDocumentListEntry(child);
-    returnString += "</ul>";
-    returnString += "</li>";
-    return returnString;
+
+function getEligibleTypesByLimit(type) {
+    let designatedChildTypes = ["class", "subclass", "formation"];
+    if (type == "Division")
+        designatedChildTypes.push(...["division"]);
+    else if (type == "Macrogroup")
+        designatedChildTypes.push(...["division", "macrogroup"]);
+    else if (type == "Group")
+        designatedChildTypes.push(...["division", "macrogroup", "group"]);
+    else
+        return [];
+
+    return designatedChildTypes;
+}
+
+function getElementsByContent(content) {
+    const element = hierarchy.filter(i => i.hierarchyName == content.content)[0];
+    let elements = [ element ];
+    if (content.descendantLimitType == "None")
+        return elements;
+
+    let designatedChildTypes = getEligibleTypesByLimit(content.type);
+
+    for (const child of element.children)
+        elements = getDescendantElementsByType(elements, child, designatedChildTypes);
+
+    return elements;
+}
+
+function generateDocumentHeader(content) {
+    const headerText = content.content;
+    const headerLevel = content.level;
+
+    let html = `
+        <h${headerLevel}>
+            ${headerText}
+        </h${headerLevel}>
+    `;
+
+    return html
+}
+
+function generateDocumentSkeletal(content, sectionContent) {
+    if (!content.content)
+        return "";
+
+    let html = "<ul class='skeletal-list-root'>";
+
+    for (const item of sectionContent.filter(i => i.type == "element")) {
+        const element = hierarchy.filter(i => i.hierarchyName == item.content)[0];
+        html += generateDocumentNamesForElement(element, item.descendantLimitType);
+    }
+
+    html += "</ul>";
+
+    return html;
+}
+
+function generateDocumentNamesForElement(element, descendantLimitType) {
+    let html = `
+        <li class='skeletal-container'>
+            <span>${element.hierarchyName}</span>
+            <span class='separator'></span>
+            <span>A</span>
+        </li>
+    `;
+
+    if (element.children.length > 0) {
+        html += "<ul>";
+        const designatedTypes = getEligibleTypesByLimit(descendantLimitType);
+        for (const child of element.children) {
+            if (!designatedTypes.includes(child.node.level))
+                continue;
+            html += generateDocumentNamesForElement(child, descendantLimitType);
+        }
+        html += "</ul>";
+    }
+
+    return html;
+}
+
+function generateDocumentText(content) {
+    const text = content.content;
+
+    let html = `
+        <p>
+            ${text}
+        </p>
+    `;
+
+    return html
 }
 
 function getDocumentHeaderTagForElement(element) {
