e76fc38876ce77d5e33bd7e4b13af3802011fe30
Kelvyn Meyers <72825162+Kelvyn-Meyers@users.noreply.github.com>
2025-03-20 10:36:54 -0700

    Fixed null elevation & BALIVE values
    Just to try and avoid future errors, we'll now replace all null numerical values with zero for parsing purposes

--- nvcs-dev/nvcs/key_western_us.py
+++ nvcs-dev/nvcs/key_western_us.py
@@ -14,8 +14,8 @@ class Plot:
         self.attrs['plantation'] = plantation
         self.attrs['hydric'] = hydric
         self.attrs['riverine'] = riverine
-        self.attrs['elevation'] = float(elevation)
-        self.attrs['balive'] = float(balive)
+        self.attrs['elevation'] = float(elevation or 0)
+        self.attrs['balive'] = float(balive or 0)
         self.attrs['fbcov'] = float(fbcov or 0)
         self.attrs['grcov'] = float(grcov or 0)
         self.attrs['shcov'] = float(shcov or 0)
