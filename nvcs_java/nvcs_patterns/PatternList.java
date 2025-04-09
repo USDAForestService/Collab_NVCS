2499b05072b4dfb31581f0c880b5a5fc923e2596
Kelvyn Meyers <72825162+Kelvyn-Meyers@users.noreply.github.com>
2025-04-09 10:51:16 -0700

    Added PatternList.java
    This ends up making use of the PatternGroup which internally uses Pattern. I may need to revisit the pattgrps constructor if it gives issues.

---
+++ nvcs_java/nvcs_patterns/PatternList.java
@@ -0,0 +1,46 @@
+package nvcs_java.nvcs_patterns;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PatternList {
+
+    public String label;
+    public List<PatternGroup> alternatives;
+
+    public PatternList(String label, Map<String,String>... pattgrps) {
+        if (pattgrps == null) {
+            throw new RuntimeException("PatternList is empty");
+        }
+        this.label = label;
+        this.alternatives = new ArrayList<>();
+        for (Map<String,String> pattgrp : pattgrps) {
+            PatternGroup entry = new PatternGroup(pattgrp);
+            alternatives.add(entry);
+        }
+    }
+
+    public boolean match_any(Map<String,String> valugrp) {
+        boolean result;
+        if (alternatives == null) {
+            result = true;
+        }
+        else {
+            result = false;
+            for (PatternGroup patterngroup : alternatives) {
+                if (patterngroup.match_all(valugrp)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("PatternList(%s, %s)", label, alternatives);
+    }
+
+}
